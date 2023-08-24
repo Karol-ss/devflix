551dbdaf06109e4a5ce5ac748bc045d55154fb76
Karol-ss <KarolSENAI1234@gmail.com>
2023-08-24 16:41:44 -0300

    chave de api trocada

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     const [searchTerm, setSearchTerm] = useState("");
     const [movies, setMovies] = useState([]);
 
-    const apiKey = "e4d577fa";
+    const apiKey = "58c4d213";
     const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
 
     useEffect(() => {
